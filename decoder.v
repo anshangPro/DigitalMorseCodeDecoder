ce589f4c5e21d201923e60f9e93e4e6871797adb
anshangPro <ay8714368@126.com>
2021-12-03 14:17:10 +0800

    fix again
    No !!!! do not fix anymore!!!!!!!!!!!!!!!

--- decoder.v
+++ decoder.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
 module decoder(
+input en,
 input clk,
 input rst,
 input [3:0] key,
--- key_board.v
+++ key_board.v
@@ -1,11 +1,22 @@
 `timescale 1ns / 1ps
 
 module key_board(
-input clk,
-input rst,
-input [3:0] row,
-output reg [3:0] col,
-output reg [3:0] key
-    );
+    input clk,
+    input rst,
+    input [3:0] row,
+    output reg [3:0] col,
+    output reg [3:0] key
+);
+
+reg [19:0] cnt;
+wire key_clk;
+
+always @ (posedge clk or posedge rst)
+    if(rst)
+      cnt <= 0;
+    else
+      cnt <= cnt + 1'b1;
+
+
 
 endmodule
