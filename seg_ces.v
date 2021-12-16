ea1bfb8a61fd0604f495b4479ba747cd00a8cea1
anshangPro <ay8714368@126.com>
2021-12-16 15:48:03 +0800

    Update seg_ces.v
    hotfix

--- seg_ces.v
+++ seg_ces.v
@@ -65,14 +65,14 @@ end
 
 always @ (posedge clk_fast) begin
     case (cnt)
-        3'b001: seg_out = seg_in[7:0];
-        3'b010: seg_out = seg_in[15:8];
-        3'b011: seg_out = seg_in[23:16];
-        3'b100: seg_out = seg_in[31:24];
-        3'b101: seg_out = seg_in[39:32];
-        3'b110: seg_out = seg_in[47:40];
-        3'b111: seg_out = seg_in[55:48];
-        3'b000: seg_out = seg_in[63:56];
+        3'b111: seg_out = seg_in[7:0];
+        3'b000: seg_out = seg_in[15:8];
+        3'b001: seg_out = seg_in[23:16];
+        3'b010: seg_out = seg_in[31:24];
+        3'b011: seg_out = seg_in[39:32];
+        3'b100: seg_out = seg_in[47:40];
+        3'b101: seg_out = seg_in[55:48];
+        3'b110: seg_out = seg_in[63:56];
     endcase;
 end
 endmodule
