eb882545fd1530fd61f1120abe6a53fd1049b56c
anshangPro <65436468+anshangPro@users.noreply.github.com>
2021-12-21 23:02:47 +0800

    Update mode_switch.v

--- mode_switch.v
+++ mode_switch.v
@@ -14,29 +14,29 @@ module mode_switch(
 wire clk_fast;
 wire mode_stable;
 counter fast(clk, rst, clk_fast);
-debounce modesw(clk_fast, rst, mode_sw, mode_stable);//消抖后的模式切换�?
+debounce modesw(clk_fast, rst, mode_sw, mode_stable);///消抖后的模式切换键
 //decoder(mode);
 //encoder(~mode);
 
-//模式切换，初始是编码
+//模式切换
 reg mode; 
 always @(posedge mode_stable or posedge rst) begin
     if(rst)begin 
-        mode <= 0;
-        led[23] <= 0;
+        mode = 0;
+        led[23] = 0;
     end else begin
-        mode <= mode +1;
-        led[23] <= led[23] +1;
+        mode = mode +1;
+        led[23] = led[23] +1;
     end
 end
 
-//矩阵键盘输入，flag�?1时代表按�?
+//矩阵键盘输入，flag为1时代表按下
 wire [3:0] value;
 wire key_flag;
 wire [4:0] morse_cord;
 key_board key(clk, rst, row, col, value, key_flag);
 
-//编码器部�?,高电平有�?
+//模式切换，初始是编码
 wire [63:0] seg_enc;
 encoder_controller enc(~mode, clk, rst, backspace, key_flag,encoder_switch ,value, seg_enc);
 
