1f0f34d5c17d45fc6b52f5e5659d883cf036f9c9
anshangPro <65436468+anshangPro@users.noreply.github.com>
2021-12-22 01:13:55 +0800

    Update seg_ces.v

--- seg_ces.v
+++ seg_ces.v
@@ -4,18 +4,17 @@ module seg(
 input clk,
 input rst,
 input [63:0] seg_in,
-output [7:0] seg_en,
+output reg [7:0] o_seg_en,
 output reg [7:0] seg_out
     );
 wire clk_fast;
 counter fast(clk, rst, clk_fast);
+wire clk_slow;
+counter #(20) slow(clk_fast, rst, clk_slow);
 
 reg [2:0] cnt;
-reg [7:0] o_seg_en;
 
-assign seg_en = ~o_seg_en;
-
-always @ (posedge clk_fast or posedge rst) begin
+always @ (posedge clk_slow or posedge rst) begin
     if(rst) begin
         cnt <= 3'b000;
     end
@@ -23,41 +22,38 @@ always @ (posedge clk_fast or posedge rst) begin
         if(cnt == 3'b111) cnt <= 3'b000;
         else cnt <= cnt + 3'b001;
     end 
-end
-
-always @ (posedge clk) begin
     case (cnt)
         3'b000: begin 
                     seg_out <= seg_in[63:56];
-                    o_seg_en <= 8'b1000_0000;
+                    o_seg_en <= 8'b0111_1111;
                 end
         3'b001: begin 
                     seg_out <= seg_in[55:48];
-                    o_seg_en <= 8'b0100_0000;
+                    o_seg_en <= 8'b1011_1111;
                 end
         3'b010: begin 
                     seg_out <= seg_in[47:40];
-                    o_seg_en <= 8'b0010_0000;
+                    o_seg_en <= 8'b1101_1111;
                 end
         3'b011: begin 
                     seg_out <= seg_in[39:32];
-                    o_seg_en <= 8'b0001_0000;
+                    o_seg_en <= 8'b1110_1111;
                 end
         3'b100: begin 
                     seg_out <= seg_in[31:24];
-                    o_seg_en <= 8'b0000_1000;
+                    o_seg_en <= 8'b1111_0111;
                 end
         3'b101: begin 
                     seg_out <= seg_in[23:16];
-                    o_seg_en <= 8'b0000_0100;
+                    o_seg_en <= 8'b1111_1011;
                 end
         3'b110: begin 
                     seg_out <= seg_in[15:8];
-                    o_seg_en <= 8'b0000_0010;
+                    o_seg_en <= 8'b1111_1101;
                 end
         3'b111: begin
                     seg_out <= seg_in[7:0];
-                    o_seg_en <= 8'b0000_0001;
+                    o_seg_en <= 8'b1111_1110;
                 end
     endcase
 end
