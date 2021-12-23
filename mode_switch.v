455af1dbf73e70a34adbd5d9be4c4bf9d2664769
anshangPro <65436468+anshangPro@users.noreply.github.com>
2021-12-23 14:14:37 +0800

    upload the decoder
    加把劲啊 不要在我写完之后才写完啊

--- mode_switch.v
+++ mode_switch.v
@@ -6,7 +6,7 @@ module mode_switch(
     input [3:0] row,
     input [8:0] encoder_switch,
     output [3:0] col,
-    output reg [23:0] led,
+    output [23:0] led,
     output [7:0] seg_en,
     output wire [7:0] seg_out
 );
@@ -21,13 +21,12 @@ debounce backspacebutton(clk_fast, rst, backspace, backspace_stable);//回退键
 
 //模式切换
 reg mode; 
+assign led[23] = mode;
 always @(posedge mode_stable or posedge rst) begin
     if(rst)begin 
         mode = 0;
-        led[23] = 0;
     end else begin
         mode = mode +1;
-        led[23] = led[23] +1;
     end
 end
 
@@ -40,6 +39,7 @@ key_board key(clk, rst, row, col, value, key_flag);
 //模式切换，初始是编码
 wire [63:0] seg_enc;
 encoder_controller enc(~mode, clk, rst, backspace_stable, key_flag,encoder_switch ,value, seg_enc);
+decoder dec(mode, clk, rst, value, key_flag, backspace_stable, led[8:4], led[2:0]);
 
 
 seg light(clk, rst, seg_enc, seg_en, seg_out);
--- seg_ces.v
+++ seg_ces.v
@@ -4,7 +4,7 @@ module seg(
 input clk,
 input rst,
 input [63:0] seg_in,
-output reg [7:0] o_seg_en,
+output [7:0] seg_en,
 output reg [7:0] seg_out
     );
 wire clk_fast;
@@ -13,48 +13,37 @@ wire clk_slow;
 counter #(20) slow(clk_fast, rst, clk_slow);
 
 reg [2:0] cnt;
+reg [7:0] o_seg_en;
+
+assign seg_en = ~o_seg_en;
 
 always @ (posedge clk_slow or posedge rst) begin
-    if(rst) begin
-        cnt <= 3'b000;
+    if(rst)
+        o_seg_en <= 8'b00000001;
+    else if(o_seg_en == 8'b10000000) begin
+        o_seg_en <= 8'b00000001;
     end
-    else begin
-        if(cnt == 3'b111) cnt <= 3'b000;
-        else cnt <= cnt + 3'b001;
-    end 
+    else o_seg_en <= o_seg_en<<1;
+end
+
+always @ (posedge clk_slow or posedge rst)begin
+    if(rst)
+        cnt <= 3'b000;
+    else if(cnt == 3'b111)
+        cnt <= 3'b000;
+    else cnt <= cnt + 3'b001;
+end
+
+always @ (posedge clk_slow) begin
     case (cnt)
-        3'b000: begin 
-                    seg_out <= seg_in[63:56];
-                    o_seg_en <= 8'b0111_1111;
-                end
-        3'b001: begin 
-                    seg_out <= seg_in[55:48];
-                    o_seg_en <= 8'b1011_1111;
-                end
-        3'b010: begin 
-                    seg_out <= seg_in[47:40];
-                    o_seg_en <= 8'b1101_1111;
-                end
-        3'b011: begin 
-                    seg_out <= seg_in[39:32];
-                    o_seg_en <= 8'b1110_1111;
-                end
-        3'b100: begin 
-                    seg_out <= seg_in[31:24];
-                    o_seg_en <= 8'b1111_0111;
-                end
-        3'b101: begin 
-                    seg_out <= seg_in[23:16];
-                    o_seg_en <= 8'b1111_1011;
-                end
-        3'b110: begin 
-                    seg_out <= seg_in[15:8];
-                    o_seg_en <= 8'b1111_1101;
-                end
-        3'b111: begin
-                    seg_out <= seg_in[7:0];
-                    o_seg_en <= 8'b1111_1110;
-                end
+        3'b111: seg_out = seg_in[7:0];
+        3'b000: seg_out = seg_in[15:8];
+        3'b001: seg_out = seg_in[23:16];
+        3'b010: seg_out = seg_in[31:24];
+        3'b011: seg_out = seg_in[39:32];
+        3'b100: seg_out = seg_in[47:40];
+        3'b101: seg_out = seg_in[55:48];
+        3'b110: seg_out = seg_in[63:56];
     endcase
 end
 endmodule
