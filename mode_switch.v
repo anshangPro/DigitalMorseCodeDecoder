dee299598ac8703358b23c75fc62d4cbdeca521d
anshangPro <65436468+anshangPro@users.noreply.github.com>
2021-12-24 18:04:24 +0800

    Update mode_switch.v

--- mode_switch.v
+++ mode_switch.v
@@ -12,13 +12,14 @@ module mode_switch(
 );
 
 wire clk_fast;
-wire mode_stable;
+wire mode_stable, backspace_stable;
 counter fast(clk, rst, clk_fast);
-debounce modesw(clk_fast, rst, mode_sw, mode_stable);//消抖后的模式切换�?
+debounce modesw(clk_fast, rst, mode_sw, mode_stable);///消抖后的模式切换键
+debounce backspacebutton(clk_fast, rst, backspace, backspace_stable);//回退键除抖
 //decoder(mode);
 //encoder(~mode);
 
-//模式切换，初始是编码
+//模式切换
 reg mode; 
 assign led[23] = mode;
 always @(posedge mode_stable or posedge rst) begin
@@ -29,20 +30,19 @@ always @(posedge mode_stable or posedge rst) begin
     end
 end
 
-//矩阵键盘输入，flag�?1时代表按�?
+//矩阵键盘输入，flag为1时代表按下
 wire [3:0] value;
 wire key_flag;
 wire [4:0] morse_cord;
 key_board key(clk, rst, row, col, value, key_flag);
 
-//编码器部�?,高电平有�?
+//模式切换，初始是编码
 wire [63:0] seg_enc;
-
 encoder_controller enc(~mode, clk, rst, backspace_stable, key_flag,encoder_switch ,value, seg_enc);
 decoder dec(mode, clk, rst, value, key_flag, backspace_stable, led[8:4], led[2:0]);
 
 
-seg light(clk_fast, rst, seg_enc, seg_en, seg_out);
+seg light(clk, rst, seg_enc, seg_en, seg_out);
 
 
 endmodule
